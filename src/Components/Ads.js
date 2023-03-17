9c25c100581cd660b118a0a5934c4e8e838d9346
Nikin Baidar <nikinbaidarr@gmail.com>
2023-03-17 16:35:58 +0545

    Added a buy me a coffee link

--- src/Components/Ads.js
+++ src/Components/Ads.js
@@ -1,15 +1,19 @@
 import  React from 'react';
 
 class Advertisements extends React.Component {
+
     render() {
         return(
-            <>
-            <div id="rightsidebar">
-            <div className="addspace">
-            <p>Ad Space?</p>
+            <div id="ads">
+
+            <a href="https://bit.ly/3Fw7B3R">
+                <div className="coffee">
+                <img src={require("../Images/BuyMeACoffee.jpg")} alt=""/>
+                <p>A cup of coffee and your support keeps me fueled!</p> 
+                </div>
+            </a>
+
             </div>
-            </div>
-            </>
         );
     }
 }
